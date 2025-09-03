65d484b7d529635ea95b9740b34c1ce04c5d7c7f
Danny2two <danthedestroy3r@gmail.com>
2025-09-03 01:12:45 -0500

    Tamed the if statement monster

--- VectorApproach.py
+++ VectorApproach.py
@@ -1,34 +1,21 @@
 import numpy as np
-#Parse List
 #Path to words list
 ValidWordsPath= './valid_wordle_words.txt'
 
-green = 1
-yellow = 0.5
-
 class VectorizedWord:
     def __init__(self,word: str):
-        self.location = np.zeros((26,5))
-
-    def get_loc(self):
-        return self.location
-
-class IVectorizedWord(VectorizedWord):
-    def __init__(self,word: str):
-        self.location = np.zeros((26,5))
-        index = 0
-        for ch in word:
-            letter = ord(ch)
-            indexOfLetter = letter - 97
-            self.location[indexOfLetter][index] = 1
-            index += 1
-        pass
+        self.location = np.zeros((26 *5, 1))
 
     def get_loc(self):
         return self.location
     
 class IIVectorizedWord(VectorizedWord):
-    def __init__(self,word):
+    def __init__(self,word:str):
+        """Vector representation of a 5 letter string in 130d vector space
+
+        Args:
+            word (str): 5 letter string used to generate vector 
+        """
         self.word=word
         self.location = np.ones((26 * 5,1))
         self.location *= -1
@@ -41,16 +28,31 @@ class IIVectorizedWord(VectorizedWord):
         pass
 
     def get_loc(self):
+        """Returns the vector pointing to this word in vector space
+
+        Returns:
+            ndarray: 130x1 vector encoding the location of the word
+        """
         return self.location
-    
+
+    def __str__(self):
+        str1 = str(self.location[0:26]).replace("\n", "")
+        str2 = str(self.location[26:(26*2) ]).replace("\n", "")
+        str3 = str(self.location[(26*2) :(26*3) ]).replace("\n", "")
+        str4 = str(self.location[(26*3) :(26*4) ]).replace("\n", "")
+        str5 = str(self.location[(26*4) :(26*5) ]).replace("\n", "")
+        return "Slot 1: " + str1 + "\nSlot 2: " + str2 + "\nSlot 3: " + str3 +"\nSlot 4: " + str4 +"\nSlot 5: " + str5
+        
 class IIVectorizedGuess(VectorizedWord):
     def __init__(self,word:str,yellow:str,grey: list[str]):
-        '''
-        word: known good letters in their spot ie "BO__Y"
-        yellow: yellow letters in their spot ie "__GG_"
-        grey: list of known false letters ie [X, Z, U]
-        '''
-        self.location = np.zeros((26 * 5,1)) #will be venctor of known good "starting point"
+        """Vector representation of a guess, continually updated with new information
+
+        Args:
+            word (str): 5 letter string of KNOWN letters ie the green results from wordle. Fill in unknown letters with an underscore.
+            yellow (str): 5 letter string of yellow letters from wordle. Fill in unknown letters with an underscore.
+            grey (list[str]): list of strings, each string being a known grey result from wordle. 
+        """
+        self.location = np.zeros((26 * 5,1))
         self.green = ["_"]*5
         self.yellow = []
         self.grey = grey
@@ -59,6 +61,11 @@ class IIVectorizedGuess(VectorizedWord):
         self.addGrey(grey)
 
     def addGreens(self,greenSTR: str):
+        """Adds known values to this words vector. 
+
+        Args:
+            greenSTR (str): 5 letter string of KNOWN letters ie the green results from wordle. Fill in unknown letters with an underscore.
+        """
         index = 0
         for ch in str(greenSTR):
             if ch == "_":
@@ -68,10 +75,9 @@ class IIVectorizedGuess(VectorizedWord):
                 if ch in self.yellow:
                     self.yellow.remove(ch)
                 self.green[index] = ch
-                letter = ord(ch)
-                indexOfLetter = letter - 97
+                indexOfLetter = get_letter_index(ch)
                 self.location[(26*index):(26*(index+1))] = -1 #We know the index is nothing but the letter we just found, set it all to -1
-                self.location[indexOfLetter + (26*index)] =1
+                self.location[indexOfLetter + (26*index)] =1 #Set correct dimension to 1
                 indexes = np.arange(5) * 26
                 indexes+=indexOfLetter
                 for ind in indexes:
@@ -80,6 +86,11 @@ class IIVectorizedGuess(VectorizedWord):
             index += 1
 
     def addYellows(self, yellowSTR: str):
+        """Adds yellow letters to this words vector
+
+        Args:
+            yellowSTR (str): 5 letter string of yellow letters from wordle. Fill in unknown letters with an underscore.
+        """
         #print("Adding yellows")
         index = 0
         for ch in yellowSTR:
@@ -90,19 +101,23 @@ class IIVectorizedGuess(VectorizedWord):
                 if ch not in self.yellow:
                     self.yellow.append(ch)
                     #print(self.yellow)
-                letter = ord(ch)
-                indexOfLetter = letter - 97
-                self.location[indexOfLetter + (26*index)] =-1
+                indexOfLetter = get_letter_index(ch)
+                self.location[indexOfLetter + (26*index)] =-1 #Set dimension where yellow was found to -1, we know this letter is not here
                 indexes = np.arange(5) * 26
                 indexes+=indexOfLetter
                 for ind in indexes:
-                    if self.location[ind] == 0:
+                    if self.location[ind] == 0: #If the dimension is 0 we know its not already been set by a green or grey
                         self.location[ind] = 0.5
                         
             index += 1
         #print(str(self.location))
 
     def addGrey(self, greys: list[str]):
+        """Adds known grey values to this words vector
+
+        Args:
+            greys (list[str]): List of strings containing the grey letters to be added
+        """
         #print("Adding grey")
         if greys != ['']:    
             for i in greys:
@@ -115,8 +130,7 @@ class IIVectorizedGuess(VectorizedWord):
                     #print("blank")
                     pass
                 else:
-                    letter = ord(ch)
-                    indexOfLetter = letter - 97
+                    indexOfLetter = get_letter_index(ch)
                     indexes = np.arange(5) * 26
                     indexes+=indexOfLetter
                     for i in indexes:
@@ -133,6 +147,11 @@ class IIVectorizedGuess(VectorizedWord):
             print("Empty Greys")
             
     def get_loc(self):
+        """Returns the vector pointing to this word in vector space
+
+        Returns:
+            ndarray: 130x1 vector encoding the location of the word
+        """
         return self.location
     
     def __str__(self):
@@ -143,6 +162,18 @@ class IIVectorizedGuess(VectorizedWord):
         str5 = str(self.location[(26*4) :(26*5) ]).replace("\n", "")
         return "Slot 1: " + str1 + "\nSlot 2: " + str2 + "\nSlot 3: " + str3 +"\nSlot 4: " + str4 +"\nSlot 5: " + str5
         
+def get_letter_index(ch:chr):
+    """Returns the index of a letter
+
+    Args:
+        ch (char): Character to get index of
+
+    Returns:
+        int: index of letter in alphabet.
+    """
+    letter = ord(ch)
+    indexOfLetter = int(letter - 97)
+    return indexOfLetter
 
 def eucDist(VecWord: VectorizedWord, VecWord2: VectorizedWord):
     dist = 0
@@ -151,49 +182,22 @@ def eucDist(VecWord: VectorizedWord, VecWord2: VectorizedWord):
     return np.sqrt(dist)
 
 def guessMaker(guessVec: IIVectorizedGuess, rWords: list[IIVectorizedWord]):
-    '''
-    guessVec: The current guess and the info it holds
-    rWords: remaining words list
-    '''
+    """Ranks the remaining words against the guess to determine the next closest valid guesses. Eliminates impossible words from the search list entirely. 
+
+    Args:
+        guessVec (IIVectorizedGuess): current guess
+        rWords (list[IIVectorizedWord]): List of remaining words.
+
+    Returns:
+        list[list[],list[]]: A list consisting of two elements, [0] being the ranked list of possible guesses; [1] being the remaining word list.
+    """
     guessLoc = guessVec.location
     listoWords = []
     isSort = False
     for word in rWords:
-        skip = False
-        #print("Testing Word: " + word.word)
-        
-        for i in range(len(guessVec.green)):
-            if guessVec.green[i]!= "_" and guessVec.green[i] != word.word[i]:
-                #print(word.word + " deleted for lack green")
-                rWords.remove(word)
-                skip=True
-                break
-        if not skip:
-            for c in guessVec.yellow:
-                #print("char? " + c)
-                if c not in word.word:
-                    #print(word.word + " deleted for lack yellow")
-                    rWords.remove(word)
-                    skip=True
-                    break
-        if not skip:
-            if guessVec.grey != ['']:
-                for c in guessVec.grey:
-                    if c in word.word:
-                        if c not in guessVec.green: 
-                            if c not in guessVec.yellow:
-                                print(word.word + " deleted for grey: " + c)
-                                print("Yellow: " + str(guessVec.yellow))
-                                print("Green: " + str(guessVec.green))
-                                rWords.remove(word)
-                                skip=True
-                                break
-        #print("rwords: " + str(len(rWords)))
-        if skip:
-            #print("skpping")
-            pass
-        else:
-            #print("ELSE HAPPENED")
+        if np.allclose(guessLoc,word.get_loc(),atol=1.5, rtol=0 ):
+            #print("Allclose with word: " + word.word)
+            #print(str(guessVec) + "\n" + str(word))
             testingword = word
             dist = eucDist(guessVec, testingword)
             if len(listoWords) <11:
@@ -208,6 +212,9 @@ def guessMaker(guessVec: IIVectorizedGuess, rWords: list[IIVectorizedWord]):
                         listoWords[i] = [dist, word.word]
                         #print("long but adding Adding to top 10:" + word.word)
                         break
+        else:
+            #print("Not all close")
+            pass
     if not isSort:
         listoWords.sort(key= lambda lit: lit[0],reverse=False)
     return([listoWords, rWords])
@@ -217,7 +224,10 @@ if __name__ == "__main__":
     print("Initalizing Word List.")
     #Open provided file, get word from each line, append to list of all words, close file.
     unsortedWords= []
-    file = open(ValidWordsPath)
+    try:
+        file = open(ValidWordsPath)
+    except FileNotFoundError:
+        print("Invalid File location for word list")
     for line in file:
         #unsortedWords.append(line.strip())
         unsortedWords.append(IIVectorizedWord(str(line.strip()).lower()))
